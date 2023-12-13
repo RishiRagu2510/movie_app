97887899c23cf98961c11bb74852083723311758
RishiRagu2510 <rishiragu25@gmail.com>
2023-12-13 12:32:31 +0530

    updated banner

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -15,7 +15,32 @@ function Banner() {
   return <div className='banner'>
   <div className='movie'>
     <img src='' alt='Background Image' className='bgImg' />
-  </div>
+    <div className='container-fluid'>
+    <div className='row'>
+      <div className='col-lg-6 col-md-12'>
+      <div className='content'>
+      <img src="" alt='Movie Title' className='movie-title'/>
+      <h4><span>Year</span>
+      <span><i>age</i></span>
+      <span>length</span>
+      <span>category</span>
+      </h4>
+      <p>The closest thing I have found is the minor assignment, a 
+      50-word sentence covering the week’s reading. Employed frequently—I 
+      schedule from perhaps four in a typical undergraduate seminar to as many as nine 
+      in an upper-division lecture—minor assignments are the most effective means 
+      I know for teaching students the quintessential communicative skill: get to the point!
+      </p>
+      <div className='button'>Button</div>
+      </div>
+      </div>
+      <div className='col-lg-6 col-md-12'>
+      
+      </div>
+    
+    </div>
+    </div>
+    </div>
   </div>;
 }
 
